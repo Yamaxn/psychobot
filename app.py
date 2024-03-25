bff628d819f7f8ee215c2e3622b92b8e867de5b5
Yamaxn <yaman.sawas@st.uskudar.edu.tr>
2024-03-25 21:35:29 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Define routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/Professionals')
+def professionals():
+    return render_template('professionals.html')
+
+@app.route('/Articles')
+def articles():
+    return render_template('articles.html')
+
+@app.route('/Login')
+def login():
+    return render_template('login.html')
+
+@app.route('/Terms-conditions')
+def terms():
+    return render_template('terms.html')
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
